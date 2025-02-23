58ea220ed9824d20281fc8018d794145a548e8c1
Unzor <73613908+UnzorZ@users.noreply.github.com>
2025-02-23 23:31:11 +0100

    Codigo documentado

--- src/RestauranteIsrael.java
+++ src/RestauranteIsrael.java
@@ -1,32 +1,77 @@
+/**
+ * La clase RestauranteIsrael gestiona los comensales del restaurante Israel
+ *
+ * @author Marcos
+ * @version 1.0
+ */
+
 public class RestauranteIsrael {
+    /**
+     * Representa el número de patatas
+     */
     public int p;
+    /**
+     * Representa el número de calamares
+     */
     public int c;
 
+    /**
+     * Contructor de la clase RestauranteIsrael
+     *
+     * @param a Indica el número de patatas que hay en el stock
+     * @param b Indica el número de calamares que hay en el stock
+     */
     public RestauranteIsrael(int a, int b) {
         this.p = a;
         this.c = b;
     }
 
+    /**
+     * Método por el que se conoce el número máximo de comensales en función del
+     * número de patatas en stock
+     *
+     * @return Devuelve el número de comensales
+     */
     int cantP() {
         int a = this.p * 3;
         return a;
     }
 
+    /**
+     * Método por el que se conoce el número máximo de comensales en función del
+     * número de calamares en stock
+     *
+     * @return Devuelve el número de comensales
+     */
     int cantC() {
         int ch = this.c * 6;
         return ch;
     }
 
+    /**
+     * Método por el que se añade la cantidad de calamares recibidos en un pedido
+     *
+     * @param c cantidad de calamares a añadir al stock
+     */
     public void addCalamares(int c) {
         this.c = this.c + c;
     }
 
+    /**
+     * Método por el que se añade la cantidad de patatas recibidos en un pedido
+     *
+     * @param d cantidad de patatas a añadir al stock
+     */
     public void addPatatas(int d) {
         this.p = this.p + d;
     }
 
+    /**
+     * @hidden
+     */
     public static void main(String[] args) {
         RestauranteIsrael r1 = new RestauranteIsrael(50, 60);
+
         System.out.println("Cantidad de calamares: " + r1.c);
         System.out.println("Cantidad de patatas: " + r1.p);
 
@@ -34,7 +79,6 @@ public class RestauranteIsrael {
             System.out.println("\nIsrael puede dar de comer a " + r1.cantP() + " personas con raciones de patatas máximo");
         else
             System.out.println("\nIsrael puede dar de comer a " + r1.cantC() + " personas con raciones de calamares máximo");
-
 
         r1.addCalamares(1);
         r1.addPatatas(80);
@@ -46,5 +90,6 @@ public class RestauranteIsrael {
             System.out.println("\nIsrael puede dar de comer a " + r1.cantP() + " personas con raciones de patatas máximo");
         else
             System.out.println("\nIsrael puede dar de comer a " + r1.cantC() + " personas con raciones de calamares máximo");
+
     }
 }
